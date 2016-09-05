29487f1f91a47c6ba3e4b26e05b9ca9ae54c2a3e
mesca <mesca@users.noreply.github.com>
2016-09-05 17:11:10 +0200

    Assume "Yes" on apt-get operations

--- rstudio-install.sh
+++ rstudio-install.sh
@@ -4,8 +4,8 @@ DEB="rstudio-server-0.99.903-amd64.deb"
 sudo echo -e "\n# The Comprehensive R Archive Network \ndeb https://cloud.r-project.org/bin/linux/ubuntu $VERSION/" >> /etc/apt/sources.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install r-base r-base-dev gdebi-core
+sudo apt-get upgrade -y
+sudo apt-get install -y r-base r-base-dev gdebi-core
 wget -O /tmp/$DEB https://download2.rstudio.org/$DEB
 sudo gdebi /tmp/$DEB
 sudo rstudio-server verify-installation
